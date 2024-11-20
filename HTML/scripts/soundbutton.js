a8123b9c7e4d08c84fc53bb697ae3611b78db926
dulat <bellorganaft@gmail.com>
2024-11-20 11:18:25 +0500

    Final

--- HTML/scripts/soundbutton.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const playSoundButton = document.getElementById("playSoundButton");
-    const notificationSound = document.getElementById("notificationSound");
-
-    playSoundButton.addEventListener("click", function() {
-        notificationSound.play();
-    });
-});
--- HTML/scripts/theme changer.js
+++
@@ -1,4 +0,0 @@
-function toggleTheme() {
-    document.body.classList.toggle("bg-dark-theme");
-    document.body.classList.toggle("bg-light-theme");
-}
